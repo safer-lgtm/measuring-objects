13c4ce661e557cb79b3da3d955b0fbcc03b6c659
safer-lgtm <git@code.fbi.h-da.de/safouan.erryfy>
2025-04-21 18:07:21 +0200

    Hough Transformation added

--- main.py
+++ main.py
@@ -21,17 +21,48 @@ def extract_edges(gray, thresh):
     kernel = np.ones((3, 3), np.uint8)
     _, binary = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY_INV)
     closed = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel, iterations=5)
-    edges = cv2.Canny(closed, 130, 180)
-    return binary, closed, edges
+    edges = cv2.Canny(closed, thresh, thresh + 50)
+    dilated = cv2.dilate(edges, kernel, iterations=1)
+    return binary, closed, edges, dilated
+
+# ----------------- 4. Hough-Transformation -----------------
+def apply_hough_inbus(edges, image):
+    img = image.copy()
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180,
+                            threshold=50, minLineLength=30, maxLineGap=10)
+    h, w = img.shape[:2]
+    center_x = w // 2
+    center_y = h // 2
+    selected_lines = []
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            euc_dist = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+            mx = (x1 + x2) // 2
+            my = (y1 + y2) // 2
+            # Nur Linien im Zentrum
+            if abs(mx - center_x) < 0.25 * w and abs(my - center_y) < 0.25 * h:
+                selected_lines.append((euc_dist, (x1, y1, x2, y2)))
+        # Nach Länge sortieren, größte zuerst
+        selected_lines = sorted(selected_lines, key=lambda x: x[0], reverse=True)
+        if len(selected_lines) >= 1:
+            x1, y1, x2, y2 = selected_lines[0][1]
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)  # lang = grün
+
+        if len(selected_lines) >= 2:
+            x1, y1, x2, y2 = selected_lines[-3][1]
+            cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 3)  # kurz = rot
+    return img
 
 # ----------------- Hauptablauf -----------------
 input_path = "images/inpus.jpg"
-scale_percent = 30
-thresh = 125
+scale_percent = 35
+thresh = 110
 
 image = load_and_resize(input_path, scale_percent)
 gray = preprocess_image(image)
-binary, closed, edges = extract_edges(gray, thresh)
+binary, closed, edges, dilated = extract_edges(gray, thresh)
+hough_result =  apply_hough_inbus(dilated, image)
 
 # ----------------- Darstellung -----------------
 processed_images = {
@@ -39,10 +70,11 @@ processed_images = {
     "Graustufen": gray,
     "Binarisiert": binary,
     "Closing": closed,
-    "Kanten (Canny)": edges
+    "Kanten (Canny)": edges,
+    "Hough-Linien": hough_result
 }
 
-fig, axs = plt.subplots(3, 3, figsize=(20, 12), facecolor='white')
+fig, axs = plt.subplots(3, 3, figsize=(30, 20), facecolor='white')
 axs = axs.flatten()
 
 for idx, (title, img) in enumerate(processed_images.items()):
